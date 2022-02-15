361c079638d0b1864e67587d12bb152c991ec90d
CJM1994 <connorjohnmullin@gmail.com>
2022-02-15 22:44:27 +0000

    allowing 0 inclusively

--- timer1.js
+++ timer1.js
@@ -1,6 +1,6 @@
 const timer = function (...args) { // Rest parameters allow more than 1 argument (makes array)
   for (const arg of args) {
-    if (arg > 0 && typeof arg !== 'string') {
+    if (arg >= 0 && typeof arg !== 'string') {
       setTimeout(() => {
         process.stdout.write('BEEP' + '\n');
       }, arg * 1000)
