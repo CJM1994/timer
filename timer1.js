e52674f41fc1ae773dc7fe37b5839ba7603d570d
CJM1994 <connorjohnmullin@gmail.com>
2022-02-15 22:43:11 +0000

    fixed bug where it would accept strings if they were a number

--- timer1.js
+++ timer1.js
@@ -1,6 +1,6 @@
-const timer = function (...args) {
+const timer = function (...args) { // Rest parameters allow more than 1 argument (makes array)
   for (const arg of args) {
-    if (arg > 0) {
+    if (arg > 0 && typeof arg !== 'string') {
       setTimeout(() => {
         process.stdout.write('BEEP' + '\n');
       }, arg * 1000)
@@ -8,4 +8,4 @@ const timer = function (...args) {
   }
 }
 
-timer(1, 2, undefined, 5, 'g', 10);
+timer(1, 2, undefined, 5, 'g', 10, '3');
