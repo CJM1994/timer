f3c07e959c93c794e99b4560b83626ad2f780309
CJM1994 <connorjohnmullin@gmail.com>
2022-02-15 22:11:06 +0000

    all edge cases are working

--- timer1.js
+++ timer1.js
@@ -1,16 +1,11 @@
 const timer = function (...args) {
-
   for (const arg of args) {
-
-    setTimeout(() => {
-      process.stdout.write('BEEP' + '\n');
-    }, arg * 1000)
-
+    if (arg > 0) {
+      setTimeout(() => {
+        process.stdout.write('BEEP' + '\n');
+      }, arg * 1000)
+    }
   }
-
 }
 
-timer(1, 10, 5, 7);
-
-// Implement an alarm clock / timer which will beep after a specified amount of time has passed. 
-// The user can specify an unlimited number of alarms using command line arguments
+timer(1, 2, undefined, 5, 'g', 10);
